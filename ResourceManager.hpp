a696acdd34055aba26aee36338bc17085e9a74cc
Jakub Wiecha <147273231+jakubw2001@users.noreply.github.com>
2023-11-08 11:17:03 +0100

    Update ResourceManager.hpp
    Update 1

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,29 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+    public:
+        ResourceManager()   //konstruktor domyślny, który inicjalizuje zarządzany obiekt przy pomocy domyślnego 
+                            //konstruktora klasy Resource
+        {
+            resource = new Resource{};
+        }
+        ResourceManager(const ResourceManager& resource2): resource(resource2.resource){}
+        ResourceManager& operator=(const ResourceManager& resource2)
+        {
+            if(this != &resource2)
+            {
+                resource = resource2.resource;
+            }
+            return *this;
+        }
+        ~ResourceManager()  //destruktor
+        {
+            delete resource;
+        }
+        double get()    //metoda double get(), która zwraca wynik zawołania metody get obiektu, którym zarządza
+        { 
+            return resource->get(); 
+        }
+    private:
+        Resource* resource;
 };
